b0bccaabeb54a2131f1c3dcf6a40c223dd46b432
Endel Dreyer <endel.dreyer@gmail.com>
2026-03-14 02:35:14 -0300

    feat: shield visual, turret rotation, camera zoom + mouse look-ahead

--- defold/main/tank.go
+++ defold/main/tank.go
@@ -58,3 +58,23 @@ embedded_components {
     z: 1.0
   }
 }
+embedded_components {
+  id: "shield"
+  type: "sprite"
+  data: "default_animation: \"white\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/gfx/game.atlas\"\n"
+  "}\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.3
+  }
+  scale {
+    x: 28.0
+    y: 28.0
+    z: 1.0
+  }
+}
